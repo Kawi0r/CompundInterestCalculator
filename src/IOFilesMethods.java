dcf905afa3f771e1dbb82675ea17d02a9fd39ff4
Kawi0r <seb.kawka@gmail.com>
2022-12-19 12:14:18 +0000

    added String block for profit, contribution and end balance

--- src/IOFilesMethods.java
+++ src/IOFilesMethods.java
@@ -1,24 +1,52 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.text.ParseException;
 
 public class IOFilesMethods {
     // in this class I will create methods to create report document to text file or spreadsheet document
 
-    public static void creatingTxtReport(String principal, String rate, int period, String contribution) {
+    public static void creatingTxtReport(String principal, String rate, int period, String contribution) throws ParseException {
 
         // Creating String block for report
-        String report = new StringBuilder().append("""
-                Inputs:
+        String initialReport = new StringBuilder().append("""
+                Investment report:
+                ---------------------------------------------------
                 Initial investment:\s""").append(principal).append("""
                             
                 Annual contribution:\s""").append(contribution).append("""
                             
-                Annual rate:\s""").append(rate).append(", ").append(period).append(" years of investment.").toString();
+                Annual rate:\s""").append(rate).append(", ").append(period).append(" years of investment.\n")
+                .toString();
+
+        BigDecimal balance = CalculatingMethods.calculate(principal, rate, period, contribution);
+        BigDecimal pureContributionBalance = CalculatingMethods.pureContribution(principal, 10, contribution);
+        String formatBalance = CalculatingMethods.moneyFormatter.format(balance);
+        String formatPureContribution = CalculatingMethods.moneyFormatter.format(pureContributionBalance);
+        BigDecimal periodProfit = CalculatingMethods.profitFromPeriod(formatBalance, formatPureContribution);
+        String formatPeriodProfit = CalculatingMethods.moneyFormatter.format(periodProfit);
+
+        // creating String block with profit and pure contribution
+        String reportProfit = """
+                ---------------------------------------------------
+                Balance at end of investment:\s""" +
+                formatBalance + """
+                                
+                Pure contribution:\s""" +
+                formatPureContribution + """
+                                
+                Interest profit is:\s""" +
+                formatPeriodProfit + "\n" +
+                "---------------------------------------------------";
+
 
 //        String yearByYearReport = new String();
 //        for (int i = 0; i < (period + 1); i++) {
 //        }
+
+        String report = initialReport +
+                reportProfit;
 
         File fileToCheck = new File("Reports/report.txt");
         if (!fileToCheck.exists()) {
